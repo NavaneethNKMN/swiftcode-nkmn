c2b9fe10a83be9ba89320fc5b634d2ef31635a4d
NavaneethNKMN <navaneethkumar1996@gmail.com>
2017-10-08 11:20:33 +0530

    UIChanges

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,26 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
+    $scope.messages=[
+    {
+        'sender':'USER',
+        'text':'hi',
+        'time':'10:01AM'
+    },
+    {
+            'sender':'BOT',
+            'text':'what can i do for you',
+            'time':'10:01AM'
+    },
+    {
+            'sender':'USER',
+            'text':'News about Modi',
+            'time':'10:01AM'
+    },
+    {
+            'sender':'BOT',
+            'text':'INVALID',
+            'time':'10:02AM'
+    }
+    ]
 });
